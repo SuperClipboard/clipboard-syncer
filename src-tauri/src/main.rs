5a184bd7d31d2973c97ae361740b1e8d2345f25b
jasonkayzk <271226192@qq.com>
2023-09-01 14:45:23 +0800

    init proj

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,54 +1,8 @@
-#![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
-)]
-
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide);
-    let system_tray = SystemTray::new().with_menu(tray_menu);
-
-    tauri::Builder::default()
-        .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-                // get a handle to the clicked menu item
-                // note that `tray_handle` can be called anywhere,
-                // just get an `AppHandle` instance with `app.handle()` on the setup hook
-                // and move it to another function or thread
-                let item_handle = app.tray_handle().get_item(&id);
-                match id.as_str() {
-                    "hide" => {
-                        let window = app.get_window("main").unwrap();
-                        window.hide().unwrap();
-                        // you can also `set_selected`, `set_enabled` and `set_native_image` (macOS only).
-                        item_handle.set_title("Show").unwrap();
-                    }
-                    _ => {}
-                }
-            }
-            _ => {}
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .build(tauri::generate_context!())
-        .expect("error while running tauri application")
-        .run(|_app_handle, event| match event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
-            _ => {}
-        });
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@/css/App.css';
+import {Button} from "antd";
+
+function App() {
+    return (
+        <div className="App">
+            hello
+            <Button type={"primary"}></Button>
+        </div>
+    );
+}
+
+export default App;
