48a967446ef22d1a4cb31f6d1269f620dc435a8f
jasonkayzk <271226192@qq.com>
2023-09-16 17:11:53 +0800

    feat: refactor listener

--- src-tauri/src/listener/clipboard.rs
+++ src-tauri/src/listener/clipboard.rs
@@ -31,81 +31,16 @@ impl ClipboardListener {
                 let mut need_notify = false;
                 let text = clipboard.get_text();
                 let _ = text.map(|text| {
-                    let content_origin = text.clone();
-                    let content = text.trim();
-                    let md5 = string::md5(&content_origin);
-                    if !content.is_empty() && md5 != last_content_md5 {
-                        // Has new clip contents
-                        let content_preview = if content.len() > Self::TEXT_PREVIEW_LEN {
-                            Some(content.chars().take(Self::TEXT_PREVIEW_LEN).collect())
-                        } else {
-                            Some(content.to_string())
-                        };
-
-                        let res = RecordDao::insert_if_not_exist(Record {
-                            content: content_origin,
-                            content_preview,
-                            data_type: record::DataTypeEnum::TEXT.into(),
-                            ..Default::default()
-                        });
-                        match res {
-                            Ok(_) => {
-                                need_notify = true;
-                            }
-                            Err(e) => {
-                                error!("insert record error: {}", e);
-                            }
-                        }
-                        last_content_md5 = md5;
-                    }
+                    Self::handle_text_message(text, &mut last_content_md5, &mut need_notify);
                 });
 
                 let img = clipboard.get_image();
                 let _ = img.map(|img| {
-                    let img_md5 = string::md5_by_bytes(&img.bytes);
-                    if img_md5 != last_img_md5 {
-                        // 有新图片产生
-                        let base64 = image::rgba8_to_base64(&img);
-                        let content_db = ImageData {
-                            width: img.width,
-                            height: img.height,
-                            base64,
-                        };
-                        // 压缩画质作为预览图，防止渲染时非常卡顿
-                        let jpeg_base64 = image::rgba8_to_jpeg_base64(&img, 75);
-                        let content_preview_db = ImageData {
-                            width: img.width,
-                            height: img.height,
-                            base64: jpeg_base64,
-                        };
-                        let content = json::stringify(&content_db).unwrap();
-                        let content_preview = json::stringify(&content_preview_db).unwrap();
-                        let res = RecordDao::insert_if_not_exist(Record {
-                            content,
-                            content_preview: Some(content_preview),
-                            data_type: record::DataTypeEnum::IMAGE.into(),
-                            ..Default::default()
-                        });
-                        match res {
-                            Ok(_) => {
-                                drop(img);
-                                need_notify = true;
-                            }
-                            Err(e) => {
-                                error!("insert image record error: {}", e);
-                            }
-                        }
-                        last_img_md5 = img_md5;
-                    }
+                    Self::handle_image_message(img, &mut last_img_md5, &mut need_notify);
                 });
 
-                let limit = AppConfig::latest().read().store_limit.unwrap();
-                let res = RecordDao::delete_record_with_limit(limit as usize);
-                if let Ok(success) = res {
-                    if success {
-                        need_notify = true;
-                    }
-                }
+                Self::handle_record_limit(&mut need_notify);
+
                 if need_notify {
                     GlobalHandler::push_message_to_window(MessageTypeEnum::ChangeClipBoard, "ok")
                         .unwrap();
@@ -115,18 +50,85 @@ impl ClipboardListener {
         });
     }
 
-    #[allow(dead_code)]
-    fn handle_text_message() {
-        todo!()
+    fn handle_text_message(text: String, last_content_md5: &mut String, need_notify: &mut bool) {
+        let content_origin = text.clone();
+        let content = text.trim();
+        let md5 = string::md5(&content_origin);
+        if !content.is_empty() && md5.ne(last_content_md5) {
+            // Has new clip contents
+            let content_preview = if content.len() > Self::TEXT_PREVIEW_LEN {
+                Some(content.chars().take(Self::TEXT_PREVIEW_LEN).collect())
+            } else {
+                Some(content.to_string())
+            };
+
+            let res = RecordDao::insert_if_not_exist(Record {
+                content: content_origin,
+                content_preview,
+                data_type: record::DataTypeEnum::TEXT.into(),
+                ..Default::default()
+            });
+            match res {
+                Ok(_) => {
+                    *need_notify = true;
+                }
+                Err(e) => {
+                    error!("insert record error: {}", e);
+                }
+            }
+            *last_content_md5 = md5;
+        }
     }
 
-    #[allow(dead_code)]
-    fn handle_image_message() {
-        todo!()
+    fn handle_image_message(
+        img: arboard::ImageData,
+        last_img_md5: &mut String,
+        need_notify: &mut bool,
+    ) {
+        let img_md5 = string::md5_by_bytes(&img.bytes);
+        if img_md5.ne(last_img_md5) {
+            // 有新图片产生
+            let base64 = image::rgba8_to_base64(&img);
+            let content_db = ImageData {
+                width: img.width,
+                height: img.height,
+                base64,
+            };
+            // 压缩画质作为预览图，防止渲染时非常卡顿
+            let jpeg_base64 = image::rgba8_to_jpeg_base64(&img, 75);
+            let content_preview_db = ImageData {
+                width: img.width,
+                height: img.height,
+                base64: jpeg_base64,
+            };
+            let content = json::stringify(&content_db).unwrap();
+            let content_preview = json::stringify(&content_preview_db).unwrap();
+            let res = RecordDao::insert_if_not_exist(Record {
+                content,
+                content_preview: Some(content_preview),
+                data_type: record::DataTypeEnum::IMAGE.into(),
+                ..Default::default()
+            });
+            match res {
+                Ok(_) => {
+                    drop(img);
+                    *need_notify = true;
+                }
+                Err(e) => {
+                    error!("insert image record error: {}", e);
+                }
+            }
+            *last_img_md5 = img_md5;
+        }
     }
 
-    #[allow(dead_code)]
-    fn handle_record_limit() {
-        todo!()
+    fn handle_record_limit(need_notify: &mut bool) {
+        let limit = AppConfig::latest().read().store_limit.unwrap();
+        let res = RecordDao::delete_record_with_limit(limit as usize);
+        if let Ok(success) = res {
+            if success {
+                *need_notify = true;
+            }
+        }
     }
 }
