a6c13787f23ac0860e7fbd3cfdcefdf2e7fceea3
jasonkayzk <271226192@qq.com>
2023-11-17 17:51:36 +0800

    feat: finished issue#10: Global search feature.

--- src/view/MainSpotlight.tsx
+++ src/view/MainSpotlight.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 import '@/css/MainSpotlight.css';
-import {SearchBar} from "@/components/SearchBar";
 import RecordList from "@/components/RecordList";
+import {Input} from "antd";
+import {debounce} from "lodash";
 
 export const MainSpotlight: React.FC = () => {
+
+    const [searchKeyword, setSearchKeyword] = useState("");
+    const [searchKeywordChild, setSearchKeywordChild] = useState("")
+
+    const handleDebounceSearch = useCallback(debounce((value) => {
+        console.log(`searchKeyword changed: ${value}`);
+        setSearchKeywordChild(value);
+    }, 600), []);
+
+    const handleKeywordChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        const {value} = e.target;
+        setSearchKeyword(value)
+        handleDebounceSearch(value);
+    }
+
     return (
         <div className={"spotlight"}>
-            <SearchBar/>
-            <RecordList/>
+            <Input
+                type="text"
+                className={"search-bar"}
+                value={searchKeyword}
+                onChange={e => handleKeywordChange(e)}
+                placeholder="Search..."/>
+            <RecordList searchKeyword={searchKeywordChild}/>
         </div>
     );
 };
